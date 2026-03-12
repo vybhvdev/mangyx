a1fbed9c1dc73044411b821628ad54967b0b3b97
vybhvdev <vaibhavyadav8900@gmail.com>
2026-03-12 16:29:08 +0530

    fix: ignoreBuildErrors in both configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   swcMinify: false,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'uploads.mangadex.org', pathname: '/covers/**' }],
